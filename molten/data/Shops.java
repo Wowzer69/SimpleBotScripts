2e0262b175cec16ce8831cb9ed2cbf8226cd2cc3
HolyMoly <22434975+Wowzer69@users.noreply.github.com>
2021-03-12 18:57:36 -0700

    Add files via upload

--- molten/data/Shops.java
+++ molten/data/Shops.java
@@ -12,25 +12,23 @@ import simple.robot.api.ClientContext;
 @Getter
 public enum Shops {
 
-	CATHERBY(9316, 10355, false,
+	CATHERBY(9312, 10355, false,
 			new WorldPoint[] { new WorldPoint(2808, 3439, 0), new WorldPoint(2804, 3432, 0), new WorldPoint(2803, 3420, 0),
 					new WorldPoint(2795, 3413, 0) }),
-	PORT_SARIM(9299, 26254, true,
+	PORT_SARIM(9379, 26254, true,
 			new WorldPoint[] { new WorldPoint(3045, 3235, 0), new WorldPoint(3035, 3235, 0), new WorldPoint(3027, 3234, 0),
 					new WorldPoint(3027, 3226, 0), new WorldPoint(3027, 3218, 0), new WorldPoint(3027, 3210, 0),
 					new WorldPoint(3027, 3204, 0), new WorldPoint(3031, 3198, 0), new WorldPoint(3035, 3193, 0),
 					new WorldPoint(3040, 3193, 0)
-
 			}),
-	PORT_PHASMATYS(9315, 29106, true,
+	PORT_PHASMATYS(9314, 29106, true,
 			new WorldPoint[] { new WorldPoint(3689, 3469, 0), new WorldPoint(3690, 3473, 0), new WorldPoint(3699, 3474, 0),
 					new WorldPoint(3699, 3482, 0), new WorldPoint(3699, 3490, 0), new WorldPoint(3699, 3496, 0),
 					new WorldPoint(3702, 3500, 0) }),
-	PORT_KHAZARD(9314, 26707, false, new WorldPoint[] {
+	PORT_KHAZARD(9317, 26707, false, new WorldPoint[] {
 
 			new WorldPoint(2661, 3159, 0), new WorldPoint(2662, 3154, 0), new WorldPoint(2667, 3149, 0),
 			new WorldPoint(2673, 3148, 0) })
-
 	;
 
 	private int npcId, bankId;
